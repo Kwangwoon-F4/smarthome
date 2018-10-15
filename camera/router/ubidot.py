4299fc8908af13ca85f31b6e6e180587cd1f8adc
root <yicho93@naver.com>
2018-10-15 10:02:43 +0000

    change router

--- camera/router/ubidot.py
+++ camera/router/ubidot.py
@@ -1,14 +1,19 @@
 from ubidots import ApiClient
 
-def send_face(user_name):
-    api = ApiClient(token="BBFF-GfqBNxadAnLhwdTjCV0frdTPKwnnK6")
-
-    user_face = api.get_variable('5bc2aba51d847271c8bac044')
-
-    change_value = user_face.save_value({
-        'value': 00,
-        'context': {
-            'user_name': user_name,
-        },
-        'description': 'The face of recognized person is ' + user_name
-    })
+class ubidot:
+	def __init__(self):
+		self.user_name = ''
+		self.api = ApiClient(token="BBFF-GfqBNxadAnLhwdTjCV0frdTPKwnnK6")
+	
+	def send_face(name):
+		user_face = self.api.get_variable('5bc2aba51d847271c8bac044')
+		self.user_name = name
+	
+		if(self.user_name != ''):
+			change_value = user_face.save_value({
+				'value': 00,
+				'context': {
+					'user_name': self.user_name,
+				},
+				'description': 'The face of recognized person is ' + self.user_name
+			})
