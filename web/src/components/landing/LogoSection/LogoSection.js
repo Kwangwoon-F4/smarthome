34a508fba7a5c70a7777740ed3495ae2272f1be7
zerone2 <yicho93@naver.com>
2018-11-04 00:57:24 +0900

    circle slider sample add

--- web/src/components/landing/LogoSection/LogoSection.js
+++ web/src/components/landing/LogoSection/LogoSection.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import logo from 'static/images/logo.png';
 import './LogoSection.scss';
 
 class LogoSection extends Component {
+
     render() {
         return (
             <div className="logo">
-                <img src={logo} alt="logo"/>
+                <Link to="/">
+                    <img src={logo} alt="logo" />
+                </Link>
             </div>
         )
     }
