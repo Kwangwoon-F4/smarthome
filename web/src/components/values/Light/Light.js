bb0e1e12ad4cc44931a628c46a2f66fab2936a68
zerone2 <yicho93@naver.com>
2018-11-06 05:53:17 +0900

    modals added

--- web/src/components/values/Light/Light.js
+++ web/src/components/values/Light/Light.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import lightImg from 'static/images/light-bulb.png';
+import Modal from '../Modal';
 
 class Light extends Component {
     state = {
-        light: 2
+        modal: false,
+        type: "조도",
+        value: 2
+    };
+    handleToggleModal = (
+        data
+    ) => {
+        this.setState({
+            modal: false,
+            data
+        });
+        console.log(this.state.type + " : "  + data.value);
+    };
+
+    showModal = () => {
+        this.setState({
+            modal: true
+        });
     };
 
     render() {
         return (
             <div className="valueContainer">
                 <div className="valueImg">
-                    <img src={lightImg} alt="light"/>
+                    <img src={lightImg} alt="light" onClick={this.showModal}/>
                 </div>
+                {this.state.modal && (
+                    <Modal
+                        onClose={this.handleToggleModal}
+                        type={this.state.type}
+                        value={this.state.value}
+                    />
+                )}
                 <div className="valueName">
                     Light
                 </div>
--- web/src/components/values/LightColor/LightColor.js
+++ web/src/components/values/LightColor/LightColor.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import rgbImg from 'static/images/rgb.png';
+import Modal from '../Modal';
 
 class LightColor extends Component {
     state = {
-        rgb: "#ffffff"
+        modal: false,
+        type: "빛 색깔",
+        value: "#ffffff"
+    };
+    handleToggleModal = (
+        data
+    ) => {
+        this.setState({
+            modal: false,
+            data
+        });
+        console.log(this.state.type + " : "  + data.value);
+    };
+
+    showModal = () => {
+        this.setState({
+            modal: true
+        });
     };
 
     render() {
         return (
             <div className="valueContainer">
                 <div className="valueImg">
-                    <img src={rgbImg} alt="light-color"/>
+                    <img src={rgbImg} alt="light-color" onClick={this.showModal}/>
                 </div>
+                {this.state.modal && (
+                    <Modal
+                        onClose={this.handleToggleModal}
+                        type={this.state.type}
+                        value={this.state.value}
+                    />
+                )}
                 <div className="valueName">
                     Light Color
                 </div>
