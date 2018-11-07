fee7c8bd4e7ec168f6b070d07d965556a28fa7da
zerone2 <yicho93@naver.com>
2018-11-07 20:17:32 +0900

    python router code update with restful

--- camera/code/02_face_training.py
+++ camera/code/02_face_training.py
@@ -1,4 +1,4 @@
-import cv2 
+import cv2
 import numpy as np
 from PIL import Image
 import os
@@ -8,32 +8,29 @@ path = '../imageset/'
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 detector = cv2.CascadeClassifier("../data/haarcascade_frontalface_default.xml");
 
+
 # function to get the images and label data
 def getImageAndLabels(path):
-	imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
-	faceSamples=[]
-	ids = []
-	for imagePath in imagePaths:
-		PIL_img = Image.open(imagePath).convert('L') 
-		# convert it to grayscale
-		img_numpy = np.array(PIL_img, 'uint8')
-		id = int(os.path.split(imagePath)[-1].split(".")[1])
-		faces = detector.detectMultiScale(img_numpy)
-		for (x,y,w,h) in faces:
-			faceSamples.append(img_numpy[y:y+h,x:x+w])
-			ids.append(id)
-	return faceSamples, ids
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    faceSamples = []
+    ids = []
+    for imagePath in imagePaths:
+        PIL_img = Image.open(imagePath).convert('L')
+        # convert it to grayscale
+        img_numpy = np.array(PIL_img, 'uint8')
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces = detector.detectMultiScale(img_numpy)
+        for (x, y, w, h) in faces:
+            faceSamples.append(img_numpy[y:y + h, x:x + w])
+            ids.append(id)
+    return faceSamples, ids
+
 
 print("\n [INFO] Training faces. It will take a few seconds. Wait ...")
-faces,ids = getImageAndLabels(path)
+faces, ids = getImageAndLabels(path)
 recognizer.train(faces, np.array(ids))
 # Save the model into trainer/trainer.yml
-recognizer.write('../trainer/trainer.yml') # recognizer.save() doesn't work
+recognizer.write('../trainer/trainer.yml')  # recognizer.save() doesn't work
 # Print the number of faces trained and end program
-print("\n [INFO] {0} faces trained. trained file saved at trainer/trainer.yml. Exiting Program".format(len(np.unique(ids))))
-
-
-
-
-
-
+print("\n [INFO] {0} faces trained. trained file saved at trainer/trainer.yml. Exiting Program".format(
+    len(np.unique(ids))))
